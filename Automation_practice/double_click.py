2f1d109c9ac36c0269b1ac54ba3786968aaebec6
Muskaan Sahu <muskaans1341@gmail.com>
2025-11-07 17:44:09 +0530

    double_click.py

--- Automation_practice/double_click.py
+++ Automation_practice/double_click.py
@@ -12,21 +12,49 @@ driver.maximize_window()
 wait = WebDriverWait(driver, 20)    
 action = webdriver.ActionChains(driver)
 
+# def double_click():
+#     button = wait.until(EC.element_to_be_clickable((By.XPATH, '//*[@id="HTML10"]/div[1]/button')))
+#     action.double_click(button).perform()
+    
+
+# # def right_click():
+# #     button = wait.until(EC.element_to_be_clickable((By.ID, 'field2')))
+# #     action.context_click(button).perform()
+# #     print("Button right clicked successfully.")
+
+# try:
+#     double_click()
+#     print("Button double clicked successfully.")
+#     # right_click()
+# except Exception as e:      
+#     print("Test Failed:", e)
+# finally:  
+#     time.sleep(2)
+#     driver.quit()
+
+
+
 def double_click():
-    button = wait.until(EC.element_to_be_clickable((By.XPATH, '//*[@id="HTML10"]/div[1]/button')))
-    action.double_click(button).perform()
-    print("Button double clicked successfully.")
+    try:
+        
+        button = wait.until(EC.element_to_be_clickable((By.XPATH, '//*[@id="HTML10"]/div[1]/button')))
+     
+        action.double_click(button).perform()
+        print("Button double clicked.")
+        
+        time.sleep(1)  
+        result = driver.find_element(By.ID, "field2").get_attribute("value")
+    
+        if result == "Hello World!":
+            print("Double click action verified successfully.")
+        else:
+            print("Double click action failed — expected result not found.")
 
-# def right_click():
-#     button = wait.until(EC.element_to_be_clickable((By.ID, 'field2')))
-#     action.context_click(button).perform()
-#     print("Button right clicked successfully.")
+    except Exception as e:
+        print("Test Failed:", e)
 
-try:
-    double_click()
-    # right_click()
-except Exception as e:      
-    print("Test Failed:", e)
-finally:  
-    time.sleep(2)
-    driver.quit()
+    finally:
+        time.sleep(2)
+        driver.quit()
+
+double_click()
